3d7f472fa8adc715d234bef2b97f89e0ee4f0c61
Present <trgu228@gmail.com>
2025-11-28 22:36:30 +0800

    Delete project/src directory

--- project/src/advanced.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import GradientBoostingClassifier
-from xgboost import XGBClassifier
-
-def get_advanced_models():
-return {
-"Gradient Boosting": GradientBoostingClassifier(),
-"XGBoost": XGBClassifier(),
-}
